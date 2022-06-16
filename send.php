4f5d48b90f223b57268e1223a51cf4de29e35a62
DanilTry <105858334+DanilTry@users.noreply.github.com>
2022-06-16 13:42:35 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,21 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("vilnifund@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: gainbuff@gmail.com \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+
+
+ini_set('display_errors','On');
+error_reporting('E_ALL');?>
